edf2a81ab768861fda70a53b443a787eb8b1ae08
Aleksey <J2k_pot@mail.ru>
2022-09-29 08:44:20 +0300

    add 25

---
+++ Task_25/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+void Degree(int a, int b)
+{
+    if (b > 0)
+    {
+        int number = 1;
+        for (int i = 1; i <= b; i++)
+        {
+            number = number * a;
+            Console.WriteLine($"число {a} в степени {i} = {number}");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("Введено неверное значение");
+    }
+
+}
+
+Degree(a, b);
