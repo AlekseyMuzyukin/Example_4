39a8f82c7ee7d712ae6d09742e7a66fae04219e2
Aleksey <J2k_pot@mail.ru>
2022-09-29 08:46:22 +0300

    add 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int NumName(int num)
+{
+    int counter = Convert.ToString(num).Length; ;
+    int sum = 0;
+    int number = 0;
+    for (int i = 0; i < counter; i++)
+    {
+        number = num % 10;
+        sum = sum + number;
+        num = num / 10;
+    }
+    return sum;
+}
+int result = NumName(num);
+Console.WriteLine($"Сумма цифр числа {num} = {result}");
